8e83fb72b66bb022e72f597c76d655051aaadc36
mehakrafiq1 <168480336+mehakrafiq1@users.noreply.github.com>
2024-11-20 22:21:22 +0500

    first commit

---
+++ app/Components/About.tsx
@@ -0,0 +1,8 @@
+function About(){
+    return(
+        <div>
+            <h1>Hello World </h1>
+        </div>
+    )
+}
+export default About
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from "./Components/Header";
+import About from "./Components/About";
+
+
+const home=()=>{
+  return(
+    <div>
+      <h1>HEllo World of Next.js</h1>
+      <p>This is Mehak Rafiq</p>
+      <pre>Sir Asharib</pre>
+        <header/>
+      <main>
+        <About/>
+      </main>
+    </div>
+  );
+}
+
+export default home
